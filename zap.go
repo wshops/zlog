d90b314f9cf1dcbd47ec9102cd688bf877ff03c1
Dr-dddd <1620210600@qq.com>
2023-02-17 11:15:49 +0800

    :fire: new code struct

---
+++ zap.go
@@ -0,0 +1,50 @@
+package zlog
+
+import (
+	"go.uber.org/zap"
+)
+
+type ZapLog struct {
+	SugaredLogger *zap.SugaredLogger
+	RegularLogger *zap.Logger
+	Cfg           *zap.Config
+}
+
+var zlogInstance *ZapLog
+
+type LogLevel int
+
+const (
+	LevelProd LogLevel = iota
+	LevelDev
+)
+
+func New(level ...LogLevel) *ZapLog {
+	if len(level) == 0 {
+		level = append(level, LevelProd)
+	}
+	zaplog := &ZapLog{}
+	var logCfg zap.Config
+	if level[0] == LevelProd {
+		logCfg = zap.NewProductionConfig()
+	} else {
+		logCfg = zap.NewDevelopmentConfig()
+	}
+	zaplog.Cfg = &logCfg
+	logger, err := zaplog.Cfg.Build()
+	if err != nil {
+		panic(err)
+	}
+	zaplog.SugaredLogger = logger.Sugar()
+	zaplog.RegularLogger = logger
+	zlogInstance = zaplog
+	return zlogInstance
+}
+
+func Log() *zap.SugaredLogger {
+	return zlogInstance.SugaredLogger
+}
+
+func GetLogger() *zap.Logger {
+	return zlogInstance.RegularLogger
+}
